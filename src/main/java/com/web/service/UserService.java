316c51737cc031242aa95ce8e0c6636b33fce4aa
johnUfo <muydinovumidjon17@gmail.com>
2024-10-31 07:43:00 +0500

    register.html and register controller is done

--- src/main/java/com/web/service/UserService.java
+++ src/main/java/com/web/service/UserService.java
@@ -1,8 +1,13 @@
 package com.web.service;
 
 import com.web.dto.RegistrationDto;
+import com.web.entity.User;
+import jakarta.validation.constraints.NotEmpty;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
 
+    User findByEmail(@NotEmpty(message = "Email should not be empty")String email);
+
+    User findByUsername(@NotEmpty(message = "Username should not be empty") String username);
 }
--- src/main/java/com/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/web/service/impl/UserServiceImpl.java
@@ -34,4 +34,14 @@ public class UserServiceImpl implements UserService {
         user.setRoles(Collections.singletonList(userRole));
         userRepository.save(user);
     }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
